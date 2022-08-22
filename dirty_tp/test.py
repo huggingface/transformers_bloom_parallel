9426563458da23bb23b608a0a6fd5fa5801f96bc
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-08-22 14:04:53 +0200

    Run a bunch of requests

--- dirty_tp/test.py
+++ dirty_tp/test.py
@@ -1,9 +1,16 @@
 import json
-from random import randrange
+from multiprocessing.pool import ThreadPool
+
 import requests
 API_URL = "http://localhost:8000/generate"
 def query(payload):
     response = requests.request("POST", API_URL, json=payload)
     return json.loads(response.content.decode("utf-8"))
-data = query({"inputs": "test", "parameters": {"max_new_tokens": 20, "do_sample": False}})
-print(data)
+
+batch_size = 32
+
+with ThreadPool(batch_size) as pool:
+    pool.map(
+        lambda : query({"inputs": "test", "parameters": {"max_new_tokens": 20, "do_sample": False}}),
+        range(batch_size)
+    )
